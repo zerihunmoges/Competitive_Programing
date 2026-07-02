78120d86a6147fcf744c760e1f2456f2a140050e
Zerihun <91532718+zerihunmoges@users.noreply.github.com>
2026-07-02 12:13:59 +0300

    Time: 601 ms (18.87%), Space: 23.2 MB (20.75%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.py
@@ -1,26 +1,22 @@
-from collections import deque
+from collections import defaultdict
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
         
-
-
-        dirc = [[0,1], [0,-1], [1,0], [-1,0]]
         m = len(grid)
         n = len(grid[0])
-        visited = set()
+        visited = defaultdict(lambda: defaultdict(int))
 
-        cells = deque([[0,0, health-grid[0][0]]])
 
-        while cells:
-            cur_x, cur_y, cur_health = cells.popleft()
-            if (cur_x, cur_y, cur_health) in visited:
-                continue
+        def isPossible(cur_x, cur_y, cur_health):
+            if visited[cur_x][cur_y] >= cur_health:
+                return False
 
-            visited.add((cur_x, cur_y, cur_health))
+            visited[cur_x][cur_y] = cur_health
 
             if cur_x == m-1 and cur_y == n-1:
                 return True
 
+            dirc = [[0,1], [0,-1], [1,0], [-1,0]]
             for x_dir, y_dir in dirc:
 
                 new_x = cur_x + x_dir
@@ -29,8 +25,11 @@ class Solution:
                 if 0 <= new_x < m and 0 <= new_y < n:
                     new_health = cur_health - grid[new_x][new_y]
                     if new_health >= 1:
-                        cells.append([new_x, new_y, new_health])
+                        if isPossible(new_x, new_y, new_health):
+                            return True
 
-        return False
+            return False
+
+        return isPossible(0,0,health - grid[0][0])
 
 
