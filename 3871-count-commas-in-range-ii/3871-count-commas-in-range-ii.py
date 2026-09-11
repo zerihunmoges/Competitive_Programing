415d4859241a8bd4f70e5922817473849dccd932
Zerihun <91532718+zerihunmoges@users.noreply.github.com>
2026-09-11 14:02:47 +0300

    Time: 1 ms (22.39%), Space: 19.2 MB (52.6%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.py
@@ -1,10 +1,5 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-
-        # 100,450,000,002
-        # 999,999,999
-
-
         count = 0
         current_commas = len(str(n))//3 if len(str(n))//3 != len(str(n))/3 else len(str(n))//3-1
         while current_commas >= 1:
